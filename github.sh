b211e56a488da6c71e1b333309d377f258cdbe43
Tom Ellis <tom-2013@purelyagile.com>
2017-06-29 00:15:15 +0100

    Don't hlint

--- github.sh
+++ github.sh
@@ -22,15 +22,6 @@ timer(){
     set -x
 }
 
-# make sure we hlint check before running the tests, in case they generate non-compliant hlint
-if [ "$HLINT_ARGUMENTS" = "" ]; then
-    HLINT_ARGUMENTS=.
-fi
-# Don't run on Mac, since that often exceeds download limits for hlint itself
-if [ "$OS" != "macOS" ]; then
-    curl -sSL https://raw.github.com/ndmitchell/hlint/master/misc/run.sh | sh -s $HLINT_ARGUMENTS --with-group=extra --with-group=future
-fi
-
 ghc --version
 cabal --version
 haddock --version
