b936db98d56e116930c22d5d1aecbbf90db46947
Tom Ellis <tom-2013@purelyagile.com>
2017-06-29 00:15:15 +0100

    I'm afraid I'm too scared to hlint

--- travis.sh
+++ travis.sh
@@ -34,10 +34,10 @@ timer(){
 }
 
 # make sure we hlint check before running the tests, in case they generate non-compliant hlint
-if [ "$HLINT_ARGUMENTS" = "" ]; then
-    HLINT_ARGUMENTS=.
-fi
-curl -sSL https://raw.github.com/ndmitchell/hlint/master/misc/run.sh | sh -s $HLINT_ARGUMENTS
+# if [ "$HLINT_ARGUMENTS" = "" ]; then
+#     HLINT_ARGUMENTS=.
+# fi
+# curl -sSL https://raw.github.com/ndmitchell/hlint/master/misc/run.sh | sh -s $HLINT_ARGUMENTS
 
 # Temporary until GHC 8.10 is released
 if [ "$GHCVER" = "head" ]; then
